397b31299fd40de0d7a0151babaa678bf46c272e
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2024-06-20 00:12:41 +0700

    feat: enable redux devtools extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,9 @@
-import {
-	legacy_createStore as createStore,
-	applyMiddleware,
-	compose,
-} from 'redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import rootReducers from './reducers';
 import { thunk } from 'redux-thunk';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 export const store = createStore(
 	rootReducers,
-	composeEnhancers(applyMiddleware(thunk))
+	composeWithDevTools(applyMiddleware(...[thunk]))
 );
